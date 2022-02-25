4ff644ac02276e1fd222017f4014266bd42659ad
NaorNC <81817039+NaorNC@users.noreply.github.com>
2022-02-25 12:02:36 -0800

    Add files via upload

--- server/sv_server.lua
+++ server/sv_server.lua
@@ -1,15 +1,15 @@
-NCCore = nil
-TriggerEvent('NCCore:GetObject', function(obj) NCCore = obj end)
+QBCore = nil
+TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
 
 RegisterServerEvent('nc-rental:attemptPurchase')
 AddEventHandler('nc-rental:attemptPurchase', function(car,price)
 	local src = source
-	local Player = NCCore.Functions.GetPlayer(src)
+	local Player = QBCore.Functions.GetPlayer(src)
     local cash = Player.PlayerData.money.cash
     if cash >= price then
         Player.Functions.RemoveMoney("cash",price,"rentals")
         TriggerClientEvent('nc-rental:vehiclespawn', source, car)
-        TriggerClientEvent('NCCore:Notify', src, car .. " has been rented for $" .. price .. ", return it in order to receive 50% of the total costs.", "success")
+        TriggerClientEvent('QBCore:Notify', src, car .. " has been rented for $" .. price .. ", return it in order to receive 50% of the total costs.", "success")
     else
         TriggerClientEvent('nc-rental:attemptvehiclespawnfail', source)
     end
@@ -18,29 +18,29 @@ end)
 RegisterServerEvent('nc-rental:giverentalpaperServer')
 AddEventHandler('nc-rental:giverentalpaperServer', function(model, plateText)
     local src = source
-    local PlayerData = NCCore.Functions.GetPlayer(src)
+    local PlayerData = QBCore.Functions.GetPlayer(src)
     local info = {
         label = plateText
     }
     PlayerData.Functions.AddItem('rentalpapers', 1, false, info)
-    TriggerClientEvent('inventory:client:ItemBox', src, NCCore.Shared.Items['rentalpapers'], "add")
+    TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items['rentalpapers'], "add")
 end)
 
 RegisterServerEvent('nc-rental:server:payreturn')
 AddEventHandler('nc-rental:server:payreturn', function(model)
     local src = source
-    local Player = NCCore.Functions.GetPlayer(source)
+    local Player = QBCore.Functions.GetPlayer(source)
     for k,v in pairs(Config.vehicleList) do 
         if string.lower(v.model) == model then
             local payment = v.price / 2
             Player.Functions.AddMoney("cash",payment,"rental-return")
-            TriggerClientEvent('NCCore:Notify', src, "You have returned your rented vehicle and received $" .. payment .. " in return.", "success")
+            TriggerClientEvent('QBCore:Notify', src, "You have returned your rented vehicle and received $" .. payment .. " in return.", "success")
         end
     end
 end)
 
-NCCore.Functions.CreateCallback('nc-rental:server:hasrentalpapers', function(source, cb)
-    local Player = NCCore.Functions.GetPlayer(source)
+QBCore.Functions.CreateCallback('nc-rental:server:hasrentalpapers', function(source, cb)
+    local Player = QBCore.Functions.GetPlayer(source)
     local Item = Player.Functions.GetItemByName("rentalpapers")
     if Item ~= nil then
         cb(true)
