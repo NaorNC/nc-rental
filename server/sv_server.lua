7a311d4c0983a9cd43b71cfd741c99f0cc0cce1d
NaorNC <81817039+NaorNC@users.noreply.github.com>
2022-07-25 17:05:20 +0300

    Added qb-menu support + refactor
    Added qb-menu support + refactor

--- server/sv_server.lua
+++ server/sv_server.lua
@@ -1,22 +1,19 @@
-QBCore = nil
-TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
+local QBCore = exports['qb-core']:GetCoreObject()
 
-RegisterServerEvent('nc-rental:attemptPurchase')
-AddEventHandler('nc-rental:attemptPurchase', function(car,price)
+RegisterServerEvent('qb-rental:attemptPurchase', function(car,price)
 	local src = source
 	local Player = QBCore.Functions.GetPlayer(src)
     local cash = Player.PlayerData.money.cash
     if cash >= price then
         Player.Functions.RemoveMoney("cash",price,"rentals")
-        TriggerClientEvent('nc-rental:vehiclespawn', source, car)
+        TriggerClientEvent('qb-rental:vehiclespawn', source, car)
         TriggerClientEvent('QBCore:Notify', src, car .. " has been rented for $" .. price .. ", return it in order to receive 50% of the total costs.", "success")
     else
-        TriggerClientEvent('nc-rental:attemptvehiclespawnfail', source)
+        TriggerClientEvent('qb-rental:attemptvehiclespawnfail', source)
     end
 end)
 
-RegisterServerEvent('nc-rental:giverentalpaperServer')
-AddEventHandler('nc-rental:giverentalpaperServer', function(model, plateText)
+RegisterServerEvent('qb-rental:giverentalpaperServer', function(model, plateText)
     local src = source
     local PlayerData = QBCore.Functions.GetPlayer(src)
     local info = {
@@ -26,8 +23,7 @@ AddEventHandler('nc-rental:giverentalpaperServer', function(model, plateText)
     TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items['rentalpapers'], "add")
 end)
 
-RegisterServerEvent('nc-rental:server:payreturn')
-AddEventHandler('nc-rental:server:payreturn', function(model)
+RegisterServerEvent('qb-rental:server:payreturn', function(model)
     local src = source
     local Player = QBCore.Functions.GetPlayer(source)
     for k,v in pairs(Config.vehicleList) do 
@@ -39,7 +35,7 @@ AddEventHandler('nc-rental:server:payreturn', function(model)
     end
 end)
 
-QBCore.Functions.CreateCallback('nc-rental:server:hasrentalpapers', function(source, cb)
+QBCore.Functions.CreateCallback('qb-rental:server:hasrentalpapers', function(source, cb)
     local Player = QBCore.Functions.GetPlayer(source)
     local Item = Player.Functions.GetItemByName("rentalpapers")
     if Item ~= nil then
